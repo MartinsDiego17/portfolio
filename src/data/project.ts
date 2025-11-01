976d90417cb7c41cf35c09c3fc6d04ef9a6e372d
MartinsDiego17 <martinsdiego0513@outlook.es>
2025-10-31 23:41:17 -0300

    add yourhiring proyect

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,13 +1,15 @@
 import type { Project } from "../types/Project";
-import shopzone from "../../public/pics/projects/shopzone.jpeg";
-import shopzone2 from "../../public/pics/projects/shopzone2.jpeg";
-const img_test = shopzone.src;
-const img_test2 = shopzone2.src;
+
+import shopzone from "../../public/pics/projects/shopzone_desktop.jpeg";
+import shopzone_mobile from "../../public/pics/projects/shopzone_mobile.jpeg";
+import yourhiring_mobile from "../../public/pics/projects/yourhiring_mobile.jpeg";
+import yourhiring_desktop from "../../public/pics/projects/yourhiring_desktop.jpeg";
+
 
 export const projects: Project[] = [
     {
         id: 1,
-        img: img_test,
+        img: shopzone.src,
         title: "Shopzone",
         description: "E-commerce ficticio que utiliza una API falsa de productos para gestionar un carrito de compras. Incluye pasarela de pago y está desplegado en Netlify, utilizando funciones serverless para la integración con Mercado Pago.",
         stack: ["Vite", "React", "Javascript"],
@@ -16,17 +18,18 @@ export const projects: Project[] = [
     },
     {
         id: 2,
-        img: img_test2,
-        img_mobile: img_test,
-        title: "Shopzone",
-        description: "E-commerce ficticio que utiliza una API falsa de productos para gestionar un carrito de compras. Incluye pasarela de pago y está desplegado en Netlify, utilizando funciones serverless para la integración con Mercado Pago.",
-        stack: ["Vite", "React", "Javascript"],
-        type: "Frontend"
+        img: yourhiring_mobile.src,
+        img_mobile: yourhiring_desktop.src,
+        title: "YourHiring",
+        description: "YourHiring es una plataforma de gestión de postulaciones laborales que cuenta con un sistema que le permite a cada usuario personalizar y optimizar su búsqueda laboral.",
+        stack: ["Astro", "React", "Javascript"],
+        type: "Frontend",
+        deploy_link: "https://your-hiring.vercel.app/"
     },
     {
         id: 3,
-        img: img_test2,
-        img_mobile: img_test,
+        img: shopzone_mobile.src,
+        img_mobile: shopzone.src,
         title: "Shopzone",
         description: "E-commerce ficticio que utiliza una API falsa de productos para gestionar un carrito de compras. Incluye pasarela de pago y está desplegado en Netlify, utilizando funciones serverless para la integración con Mercado Pago.",
         stack: ["Vite", "React", "Javascript"],
@@ -35,7 +38,7 @@ export const projects: Project[] = [
     },
     {
         id: 4,
-        img: img_test,
+        img: shopzone.src,
         title: "Shopzone",
         description: "E-commerce ficticio que utiliza una API falsa de productos para gestionar un carrito de compras. Incluye pasarela de pago y está desplegado en Netlify, utilizando funciones serverless para la integración con Mercado Pago.",
         stack: ["Vite", "React", "Javascript"],
