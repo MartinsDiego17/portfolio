31cdf3fb6a3807e04f0d917aad0050ffa2ece133
MartinsDiego17 <martinsdiego0513@outlook.es>
2025-11-01 18:42:27 -0300

    add projects data

--- src/data/project.ts
+++ src/data/project.ts
@@ -2,8 +2,15 @@ import type { Project } from "../types/Project";
 
 import shopzone from "../../public/pics/projects/shopzone_desktop.jpeg";
 import shopzone_mobile from "../../public/pics/projects/shopzone_mobile.jpeg";
+
 import yourhiring_mobile from "../../public/pics/projects/yourhiring_mobile.jpeg";
 import yourhiring_desktop from "../../public/pics/projects/yourhiring_desktop.jpeg";
+
+import zaporta_mobile from "../../public/pics/projects/zaporta_mobile.jpeg";
+import zaporta_desktop from "../../public/pics/projects/zaporta_desktop.jpeg";
+
+import pulsetech_mobile from "../../public/pics/projects/pulsetech_mobile.jpeg";
+import pulsetech_desktop from "../../public/pics/projects/pulsetech_desktop.jpeg";
 
 
 export const projects: Project[] = [
@@ -28,21 +35,21 @@ export const projects: Project[] = [
     },
     {
         id: 3,
-        img: shopzone_mobile.src,
-        img_mobile: shopzone.src,
-        title: "Shopzone",
-        description: "E-commerce ficticio que utiliza una API falsa de productos para gestionar un carrito de compras. Incluye pasarela de pago y está desplegado en Netlify, utilizando funciones serverless para la integración con Mercado Pago.",
-        stack: ["Vite", "React", "Javascript"],
+        img: zaporta_mobile.src,
+        img_mobile: zaporta_desktop.src,
+        title: "Landing page promocional",
+        description: "Landing page destinada a promocionar los conocimientos y servicios de un entrenador personal, destacando su experiencia, especialidades y facilitando el contacto con potenciales clientes.",
+        stack: ["Astro", "React", "Javascript"],
         type: "Frontend",
-        deploy_link: "https://shopzonestore.netlify.app/"
+        deploy_link: "https://santi-zaporta.vercel.app/"
     },
     {
         id: 4,
-        img: shopzone.src,
-        title: "Shopzone",
-        description: "E-commerce ficticio que utiliza una API falsa de productos para gestionar un carrito de compras. Incluye pasarela de pago y está desplegado en Netlify, utilizando funciones serverless para la integración con Mercado Pago.",
-        stack: ["Vite", "React", "Javascript"],
+        img: pulsetech_desktop.src,
+        title: "Pulsetech",
+        description: "Pulsetech es una plataforma que desarrollé hace un tiempo, cuando me dedicaba de manera freelance al diseño y venta de páginas web, ofreciendo soluciones personalizadas para emprendedores y profesionales.",
+        stack: ["Astro", "React", "I18N"],
         type: "Frontend",
-        deploy_link: "https://shopzonestore.netlify.app/"
+        deploy_link: "https://pulsetech-chi.vercel.app/"
     },
 ];
