3a45efdde3c14eb4f8d47cb2a436f566c2267584
MartinsDiego17 <martinsdiego0513@outlook.es>
2025-11-01 21:55:21 -0300

    change og:image metadata

--- src/data/info_metadata.ts
+++ src/data/info_metadata.ts
@@ -1,12 +1,11 @@
 import type { Metadata } from "../types/Metadata";
 const desc = "Diego Martins, software developer Full Stack, creando aplicaciones web modernas y escalables con React, Next.js y Node.js.";
-import mockup from "../../public/pics/mockup.jpg";
 
 export const info_metadata : Metadata = {
 	description: desc,
     author: "Diego Martins",
-    ogTitle: "DMGTech - Portfolio",
+    ogTitle: "DMGTech",
     ogDescription: desc,
-    ogImage: mockup.src,
+    ogImage: "https://github.com/MartinsDiego17/portfolio/blob/main/public/pics/mockup.jpg?raw=true",
     ogType: "website"
 };
